e3996172e80b3dea748673700c8dde50aa01cc2e
Laurent <chv.laurent@gmail.com>
2022-11-30 11:54:03 +0100

    Step 9 début

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,6 +1,8 @@
 
 
 let cart = []
+let quantityProductTotal = 0
+let totalProductPrice = 0
 
 fetch("http://localhost:3000/api/products/")
    .then(data => data.json())
@@ -18,74 +20,29 @@ fetch("http://localhost:3000/api/products/")
             let cartProductQuantity = product.quantity
             // console.log("Voici la quantité : " + cartProductQuantity) //#########
 
-            const calculatePriceTotal = []
-
             for (product_ in jsonListProduct) {
+               let cartProductPrice = jsonListProduct[product_].price
+
                if (referenceId == jsonListProduct[product_]._id) {
-
-
-                  let cartProductPrice = jsonListProduct[product_].price
-                  // console.log("Voici le prix : " + cartProductPrice) //#########
-
-                  // let productTotal = 0
-                  // let totalPrice = 0
-
-                  // totalsCalulated(referenceId, cartProductQuantity, cartProductPrice)
-                  // const [productTotal, totalPrice] = totalsCalulated(referenceId, cartProductQuantity, cartProductPrice)
-                  const [productTotal, totalPrice] = [0, 0]
-                  
-                  // // console.log("Voici le referenceId[product_].quantity : " + jsonListProduct[product_].quantity) //#########
-
-                  // const productTotal = jsonListProduct[product_].quantity.reduce((accumulator, currentValue) => accumulator + currentValue, initialValue)
-                  //    // referenceId[product_].quantity * lot[v].price;
-                  //    // referenceId[product_].quantity * jsonListProduct[product_].price;
-                  //    // cartProductQuantity * cartProductPrice;
-
-                  // console.log("Voici la quantité " + productTotal)
-
-                  // calculatePriceTotal.push(jsonListProduct[productTotal]);
-
-                  // const initialValue = 0
-                  // // const reducer = ((accumulator, currentValue) => accumulator + currentValue, initialValue);
-                  // // const totalPrice = calculatePriceTotal.reduce(reducer);
-                  // const totalPrice = calculatePriceTotal.reduce((accumulator, currentValue) => accumulator + currentValue, initialValue)
-                  // // const totalPrice = calculatePriceTotal;
-
-                  // console.log("Voici la quantité " + productTotal + ", pour un total de : " + totalPrice)
-                  // inserTotalsHtml(productTotal, totalPrice)
-
+                  quantityProductTotal += parseInt(cartProductQuantity)
+                  totalProductPrice += parseInt(cartProductPrice) * parseInt(cartProductQuantity)                  
                   cart.push(jsonListProduct[product_])
                   inserProductHtml(jsonListProduct[product_], referenceId, referenceColor, cartProductQuantity)
-                  inserTotalsHtml(productTotal, totalPrice)
                }
             }
          }
-   });
 
-// function totalsCalulated(reference, quantity, price){
-//    if (reference == reference){
-//       let quantityTotal = 0
-//       quantityTotal += quantity
-//       console.log("Voici la quantité totale : " + quantityTotal)
-//       // let reducer = (accumulator, curr) => accumulator + curr;
-//       const initialValue = 0
-//       let productTotal = quantityTotal.reduce(
-//          (accumulator, currentValue) => accumulator + currentValue,
-//          initialValue
-//       )
-//       // let productTotal = quantityTotal.reduce((accumulator, curr) => accumulator + curr);
-//       // let productTotal = quantityTotal.reduce((accumulator, currentValue) => accumulator + currentValue, quantityTotal)
-//       // let totalPrice = quantity * price.reduce((accumulator, currentValue) => accumulator + currentValue)
-//       // let productTotal = quantityTotal
-//       let totalPrice = quantity * price
-//       console.log("Voici la quantité " + productTotal)
-//       console.log("Voici le prix " + totalPrice)
-//       return [productTotal, totalPrice]
-//    }
-// }
+         let modifierQuantity = document.getElementsByClassName("itemQuantity")
+         for (modifierToQuantity in modifierQuantity){
+            modifierQuantity[modifierToQuantity].addEventListener("change", addModifierQuantity)
+         }
+
+         inserTotalsHtml(quantityProductTotal, totalProductPrice)
+         console.log(quantityProductTotal) //#########
+         console.log(totalProductPrice) //#########
+      });
 
 function inserProductHtml(cartProduct, referenceId, referenceColor, cartProductQuantity){
-   const cartProductPrice = cartProductQuantity * cartProduct.price
    cart__items.innerHTML += 
       `<article class="cart__item" data-id="${referenceId}" data-color="${referenceColor}">
          <div class="cart__item__img">
@@ -96,7 +53,6 @@ function inserProductHtml(cartProduct, referenceId, referenceColor, cartProductQ
                <h2>${cartProduct.name}</h2>
                <p>${referenceColor}</p>
                <p>${cartProduct.price + " €"}</p>
-               <p>${cartProductPrice + " €"}</p>
             </div>
             <div class="cart__item__content__settings">
                <div class="cart__item__content__settings__quantity">
@@ -114,6 +70,11 @@ function inserProductHtml(cartProduct, referenceId, referenceColor, cartProductQ
 function inserTotalsHtml(quantityTotal, priceTotal){
    totalQuantity.innerHTML += quantityTotal
    totalPrice.innerHTML += priceTotal
+}
+
+function addModifierQuantity(){
+   // Modifier le localstorage en faisant +/- 1
+   // Actualiser les totaux via une fonction (ou voir avec fonction inserTotalsHtml)
 }
 
 
