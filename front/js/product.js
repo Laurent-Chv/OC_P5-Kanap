174b69780ee43456a65afef93e7671e9cc9b34aa
Laurent <chv.laurent@gmail.com>
2022-12-14 16:05:40 +0100

    Add messages alerts

--- front/js/product.js
+++ front/js/product.js
@@ -10,11 +10,11 @@ if (search_params.has('id')) {
 fetch("http://localhost:3000/api/products/" + productId)
    .then(data => data.json())
    .then(jsonListProduct => {
-      document.querySelector(".item__img").innerHTML +=
+      document.querySelector(".item__img").innerHTML =
          `<img src="${jsonListProduct.imageUrl}" alt="${jsonListProduct.imageUrl}">`;
-      title.innerHTML += jsonListProduct.name;
-      price.innerHTML += jsonListProduct.price;
-      description.innerHTML += jsonListProduct.description;
+      title.innerHTML = jsonListProduct.name;
+      price.innerHTML = jsonListProduct.price;
+      description.innerHTML = jsonListProduct.description;
       for (let color of jsonListProduct.colors) {
          colors.innerHTML +=
             `<option value="${color}">${color}</option>`
@@ -36,19 +36,22 @@ function addToCart() {
 
    let newProductQuantity = newItemJSON.quantity
 
-   if (localStorage.getItem(productReference)) {
-      let currentProduct = JSON.parse(localStorage.getItem(productReference))
-      let currentProductQuantity = currentProduct[0].quantity
-      let productQuantityAdded = parseInt(currentProductQuantity) + parseInt(newProductQuantity)
-      newItemJSON = {
-         reference: productReference,
-         id: productId,
-         quantity: productQuantityAdded,
-         color: productColorsList.value
+   if (checkInput()) {
+      if (localStorage.getItem(productReference)) {
+         let currentProduct = JSON.parse(localStorage.getItem(productReference))
+         let currentProductQuantity = currentProduct[0].quantity
+         let productQuantityAdded = parseInt(currentProductQuantity) + parseInt(newProductQuantity)
+         newItemJSON = {
+            reference: productReference,
+            id: productId,
+            name: document.querySelector("#title").innerHTML,
+            quantity: productQuantityAdded,
+            color: productColorsList.value
+         }
+         increaseItem(newItemJSON, cartList, productReference)
+      } else {
+         increaseItem(newItemJSON, cartList, productReference)
       }
-      increaseItem(newItemJSON, cartList, productReference)
-   } else {
-      increaseItem(newItemJSON, cartList, productReference)
    }
 }
 
@@ -57,6 +60,12 @@ function increaseItem(newItemJSON, cartList, productReference){
    newItem = JSON.stringify(cartList)
    localStorage.setItem(productReference, newItem)
    console.log(localStorage) //#########
+   console.log(newItemJSON.name)
+   let validPorduct = confirm(`Le produit "${newItemJSON.name}" de couleur "${newItemJSON.color}" avec une quantité de "${newItemJSON.quantity}" a été ajouté au panier avec succès.\n
+   Voulez-vous aller au panier ? ou continué vos achats`)
+   if(validPorduct){
+      location.href="./cart.html"
+   }
 }
 
 window.onload = function () {
@@ -64,4 +73,17 @@ window.onload = function () {
    buttonAddToCart.addEventListener("click", addToCart, false)
 }
 
+function checkInput(){
+   
+   console.log(1 > parseInt(quantity.value))
+   if(colors.value == ""){
+      alert("Veuillez choisisr une couleur")
+      return false
+   } else if (1 > parseInt(quantity.value) || parseInt(quantity.value) > 100){
+      alert("Veuillez choisisr une quantité entre 1 et 100")
+      return false
+   } else {
+      return true
+   }
+}
 
