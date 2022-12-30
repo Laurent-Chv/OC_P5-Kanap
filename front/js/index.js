7ea1be1d2db5b8c583d68ec0930299becc201a8a
Laurent <chv.laurent@gmail.com>
2022-12-30 15:14:04 +0100

    Commentaires page d'acceuil

--- front/js/index.js
+++ front/js/index.js
@@ -1,31 +1,29 @@
-//? --------------------------
-//? ----- Insérer les produits dans la page d’Accueil de manière dynamique
-//? --------------------------
+//TODO ****************** Gestion de la page d'accueil ******************
 
-//* ----- Création d'une classe produit qui contient un constructeur de produits au format JSON (format backend)
-//* ----- Le constructeur assigne toutes les propriétés du backend à chaque objet produit
+//? ************************************
+//? Insérer les produits dans la page d’Accueil de manière dynamique
+//? ************************************
+
+//* Création d'une classe produit qui contient un constructeur qui assigne toutes les propriétés de produits au format JSON (format backend)
 class Product {
    constructor(jsonProduct) {
       jsonProduct && Object.assign(this, jsonProduct)
    }
 }
 
-//* ----- Cette classe met les produits sous forme de liste de produits
+//* Cette classe met les produits sous forme de liste de produits
 class ProductManager {
    constructor(listProduct) {
       this.listProduct = listProduct
    }
 }
 
-//* ----- Gestion de l'affichage dynamique des produits sur la page d'Accueil
-// ----- On requête le serveur afin de récupérer la liste des produits qui y sont enregistrés
+//* Gestion de l'affichage dynamique des produits sur la page d'Accueil via une requête du serveur
 fetch("http://localhost:3000/api/products")
    .then(data => data.json())
    .then(jsonListProduct => {
       for (let jsonProduct of jsonListProduct) {
-         // ----- Pour chaque produit on crée donc avec notre classe "Product", un objet produit
          let product = new Product(jsonProduct)
-         // ----- On va afficher les données sur notre page index.html en faisant des interpolations de variables 
          document.querySelector(".items").innerHTML += `<a href="./product.html?id=${product._id}">
                                                                   <article>
                                                                      <img src="${product.imageUrl}" alt="${product.altTxt}">
