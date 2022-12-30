229a57c41ce6bb8c3d986edcd50dcf775b84e703
Laurent <chv.laurent@gmail.com>
2022-12-30 16:44:02 +0100

    Affichage du numéro sans stockage + commentaires

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,10 +1,13 @@
 //TODO ****************** Gestion de la page de confirmation ******************
 
+//? ************************************
+//? Récupérer et afficher le numéro de commande
+//? ************************************
 
+//* Permet de récupérer le numéro de commande à afficher sur la page depuis l'URL
 var url = new URL(window.location);
 var search_params = new URLSearchParams(url.search);
-if (search_params.has('id')) {
-   var orderNumber = search_params.get('id');
+if (search_params.has('id')) { // Condition permettant de chercher s'il y a "id" dans l'URL et si tel est le cas, d'afficher la valeur de celui-ci (le numéro de commande) sur la page 
+   orderId.innerHTML = search_params.get('id');
 }
 
-orderId.innerHTML = orderNumber
